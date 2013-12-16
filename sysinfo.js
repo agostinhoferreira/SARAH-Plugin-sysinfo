d76f61b9f8ac35a22779437e7cc18eb02cec6877
Ferreira Agostinho <maytetino@gmail.com>
2013-12-16 11:20:06 +0100

    Update sysinfo.js
    Mis a jour nom des variables.
    Mis a jour du au bug disk.vbs

--- sysinfo.js
+++ sysinfo.js
@@ -32,19 +32,26 @@ exports.cron = function(callback, task , SARAH ){
 	  //  
 },3000);
 	// =========================================================================	
-	var mac_adres  = urlfile+"cpu.vbs";
-    var child = exec(mac_adres, function (error, stdout, stderr) {
+	var fichier_lect  = urlfile+"cpu.vbs";
+    var child = exec(fichier_lect, function (error, stdout, stderr) {
 			if (error !== null) {
 			   		console.log('cpu:' +error);
 					}
 			}); 
 	// =========================================================================	
-	var mac_adres  = urlfile+"memoire.vbs";
-    var child = exec(mac_adres, function (error, stdout, stderr) {
+	fichier_lect  = urlfile+"memoire.vbs";
+    var child = exec(fichier_lect, function (error, stdout, stderr) {
 			if (error !== null) {
 			   		console.log('cpu:' +error);
 					}
-			}); 		
+			}); 
+	// =========================================================================	
+	fichier_lect  = urlfile+"disk.vbs";
+    var child = exec(fichier_lect, function (error, stdout, stderr) {
+			if (error !== null) {
+			   		console.log('cpu:' +error);
+					}
+			}); 			
 // -------------------------------------------
 // FIN CRON
 // -------------------------------------------
@@ -61,38 +68,38 @@ exports.action = function(data, callback, config, SARAH){
   		{
 		case "MEM":
 		// Mémoire utilisé
-		var retour_etat = readfile(urlfile+"mem.txt");
-				SARAH.speak(" le poucentage et de "+retour_etat);
+		var retour_file = readfile(urlfile+"mem.txt");
+				SARAH.speak(" le poucentage et de "+retour_file);
 				
 		break;
 		
 		case "FREE":
 		// Mémoire utilisé
-		var retour_etat = readfile(urlfile+"diskl.txt");
-				SARAH.speak(" Escpace disponible et de "+retour_etat);
+		var retour_file = readfile(urlfile+"diskl.txt");
+				SARAH.speak(" Escpace disponible et de "+retour_file);
 				
 		break;
 		
 		case "DD":
 		// Mémoire utilisé
-		var retour_etat = readfile(urlfile+"diskt.txt");
-				SARAH.speak(" Escpace Total du disque et de "+retour_etat);
+		var retour_file = readfile(urlfile+"diskt.txt");
+				SARAH.speak(" Escpace Total du disque et de "+retour_file);
 				
 		break;
 		
 		case "CPU":
 		// Mémoire utilisé
-		var retour_etat = readfile(urlfile+"cpus.txt");
-				SARAH.speak(" La charge CPU et de "+retour_etat);
+		var retour_file = readfile(urlfile+"cpus.txt");
+				SARAH.speak(" La charge CPU et de "+retour_file);
 				
 		break;
 		
 		case "TCPU":
 		// Mémoire utilisé
-		var retour_etat = readfile(urlfile+"cput.txt");
+		var retour_file = readfile(urlfile+"cput.txt");
 		console.log('cpu:' +retour_etat+'<');
-				if (!retour_etat){
-				             SARAH.speak(" La Température CPU et de "+retour_etat);
+				if (!retour_file){
+				             SARAH.speak(" La Température CPU et de "+retour_file);
 									} else {
 									        SARAH.speak("Désolé cette information n'est pas disponible ");
 											}
@@ -100,8 +107,8 @@ exports.action = function(data, callback, config, SARAH){
 		
 		case "SRV":
 		// Mémoire utilisé
-		var retour_etat = readfile(urlfile+"servicenb.txt");
-				SARAH.speak("Il y a "+retour_etat+" programme et service démarrer");
+		var retour_file = readfile(urlfile+"servicenb.txt");
+				SARAH.speak("Il y a "+retour_file+" programme et service démarrer");
 		break;
 		
 		}
